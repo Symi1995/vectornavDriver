631b25ca9c53509410bd672b2557c78d7afe94fa
AnonymDavid <jodavid512@gmail.com>
2024-06-27 13:02:21 +0200

    Revert tf

--- vectornav/launch/tf_vectornav.launch.py
+++ vectornav/launch/tf_vectornav.launch.py
@@ -2,22 +2,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # vectornav_imu_tf = Node(
-    #     package='tf2_ros',
-    #     executable='static_transform_publisher',
-    #     name='golf_vectornav_base_link_tf',
-    #     arguments=['1.464', '-0.035.5', '0.258', '0', '0', '0', 'golf/vectornav_imu', 'golf/base_link'],
-    #     output='screen'
-    # )
-
-    base_link_tf = Node(
+    vectornav_tf = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='golf_vectornav_base_link_tf',
-        arguments=['-0.760', '-0.485', '-1.129', '0', '0', '0', 'golf/base_link', 'golf/vectornav'],
+        arguments=[
+            '--x',     '-1.464',
+            '--y',     '0.035.5',
+            '--z',     '-0.258',
+            '--yaw',   '0',
+            '--pitch', '0',
+            '--roll',  '0',
+
+            '--frame-id',      'golf/vectornav',
+            '--child-frame-id','golf/base_link'],
         output='screen'
     )
 
     return LaunchDescription([
-        base_link_tf
+        vectornav_tf
     ])
